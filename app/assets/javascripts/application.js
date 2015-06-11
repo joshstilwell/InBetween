aee882936db4182286341a0e39f09a62a840d52d
joshstilwell <josh.stilwell@gmail.com>
2015-06-11 14:58:26 +0000

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+$(document).ready(function (){
+
+    function initialize() {
+      var mapOptions = {
+      center: { lat: -34.397, lng: 150.644},
+      zoom: 8
+      };
+        
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);    
+    }
+      
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
